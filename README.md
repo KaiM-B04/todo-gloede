7c0bb52f1c06f7c0b3897b861ae5cfe50a805272
KaiM-B04 <git@stackblitz.com>
2025-01-08 11:29:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# todo-gloede
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/KaiM-B04/todo-gloede)
---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('add').addEventListener('click', function () {
+  let inputValue = document.getElementById('to-do').value;
+  if (inputValue) {
+    addTodo(inputValue);
+    document.getElementById('to-do').value = ' ';
+  }
+});
+
+function addTodo(task) {
+  let todoList = document.getElementById('list');
+  let li = document.createElement('li');
+  li.textContent = task;
+
+  let removeButton = document.createElement('button');
+  removeButton.textContent = 'Fjern';
+  removeButton.onClick = function () {
+    todoList.removeChild(li);
+  };
+
+  li.appendChild(removeButton);
+  li.addEventListener('click', function () {
+    li.classList.toggle('completed');
+  });
+
+  todoList.appendChild(li);
+}
